658dbaa476e590551184bd26af3f71251c21100a
Sandeep <your-email@example.com>
2025-03-19 17:11:07 +0530

    add new code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
 console.log("komal");
+let isOrderAccepted = false ;
+let isvaletFound = false ;
+
+window.addEventListener('load' , () =>{
+    document.getElementById('acceptOrder').addEventListener('click' , function (){
+        askRejectOrAccepted();
+        checkIfAcceptRject();
+    })
+
+})
+function askRejectOrAccepted() {
+    setTimeout(() => {
+        isOrderAccepted = confirm("accept or reject your order");
+        console.log(isOrderAccepted);
+        
+    }, 1000);
+}
+// step two check the order reject or accept
+function checkIfAcceptRject() {
+    
+    let promise = new Promise((resove , reject) =>{
+
+        setTimeout(() => {
+            if (checkIfAcceptRject) {
+                resove(true);
+            }
+            else{
+                reject(false)
+            }
+        }, 5000);
+
+    })
+
+   
+
+}
+
+
